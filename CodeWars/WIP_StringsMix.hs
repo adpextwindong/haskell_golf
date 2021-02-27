a60f342f9aa7e9a2c565628de46a3604499c2df6
Takumi Crary <tcrary@gmail.com>
2021-02-28 00:14:03 +0900

    Add merging to StringsMix, missing lexigraphical ordering

--- CodeWars/WIP_StringsMix.hs
+++ CodeWars/WIP_StringsMix.hs
@@ -2,27 +2,39 @@
 
 -- module Codewars.G964.Mixin where
 
+import Data.Maybe (catMaybes)
+import Data.List (sortBy, intercalate)
 import Data.Char (isLower)
 import qualified Data.Map.Strict as Map
 import qualified Data.Map.Merge.Strict as Merge
 import Control.Applicative
 
+repr :: Char -> Int -> String
+repr k v = replicate (fromIntegral v) k
+
 data Smix k v = LeftSmix k v | RightSmix k v | EqSmix k v
 
-charCountMap :: String -> Data.Map.Map Char Integer
+showSmix :: Smix Char Int -> String
+showSmix (LeftSmix k v) = "1:" ++ (repr k v)
+showSmix (RightSmix k v) = "2:" ++ (repr k v)
+showSmix (EqSmix k v) = "=:" ++ (repr k v)
+
+extractSmixPair :: Smix Char Int -> (Char, Int)
+extractSmixPair (LeftSmix k v) = (k,v)
+extractSmixPair (RightSmix k v) = (k,v)
+extractSmixPair (EqSmix k v) = (k,v)
+
+charCountMap :: String -> Map.Map Char Int
 charCountMap s = Map.fromListWith (+) $ liftA2 (,) s [1]
 
-dealWithLeft :: k -> x -> Maybe Y
-dealWithLeft k x
+--Merge handling and dropping 1s
+dealWith :: Bool -> Char -> Int -> Maybe (Smix Char Int)
+dealWith invert k x
     | x == 1 = Nothing
+    | invert = Just (RightSmix k x)
     | otherwise = Just (LeftSmix k x)
 
-dealWithRight :: k -> x -> Maybe Y
-dealWithRight k x
-    | x == 1 = Nothing
-    | otherwise = Just (RightSmix k x)
-
-dealWithBoth :: k -> x -> y -> Maybe Z
+dealWithBoth :: Char -> Int -> Int -> Maybe (Smix Char Int)
 dealWithBoth k 1 1 = Nothing
 dealWithBoth k x 1 = Just (LeftSmix k x)
 dealWithBoth k 1 y = Just (RightSmix k y)
@@ -31,17 +43,15 @@ dealWithBoth k x y
     | x > y = Just (LeftSmix k x)
     | x < y = Just (RightSmix k y)
 
---TODO test this, maybe we can even filter 1's before even merging
--- merge (mapMaybeMissing dealWithLeft)
---            (mapMaybeMissing dealWithRight)
---            (zipWithMaybeMatched dealWithBoth)
---            countMapLeft countMapRight
+smixOrdering :: Smix Char Int -> Smix Char Int -> Ordering
+smixOrdering (k1 v1) (k2 v2) = undefined
 
-
-mix :: [Char] -> [Char] -> [Char]
-mix s1 s2 = undefined
+mix :: String -> String -> String
+mix s1 s2 = intercalate "/" $ showSmix  <$> result
     where countMapLeft = charCountMap $ filter isLower s1
           countMapRight = charCountMap $ filter isLower s2
-          mergeMap = undefined --TODO handle merging then printing
-
---https://hackage.haskell.org/package/containers-0.6.4.1/docs/Data-Map-Merge-Strict.html#v:merge
+          mergeMap = Merge.merge (Merge.mapMaybeMissing (dealWith False))
+                                 (Merge.mapMaybeMissing (dealWith True))
+                                 (Merge.zipWithMaybeMatched dealWithBoth)
+                                 countMapLeft countMapRight
+          result = sortBy smixOrdering $ fmap snd $ Map.toList mergeMap
