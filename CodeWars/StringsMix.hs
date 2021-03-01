804dfb5904c207253b45a06c1ef51e29db1db698
Takumi Crary <tcrary@gmail.com>
2021-03-01 05:42:18 +0000

    Gold the maybes out of Smix

--- CodeWars/StringsMix.hs
+++ CodeWars/StringsMix.hs
@@ -20,20 +20,18 @@ charCountMap :: String -> Map.Map Char Int
 charCountMap s = Map.fromListWith (+) $ liftA2 (,) s [1]
 
 --Merge handling and dropping 1s
-dealWith :: Bool -> Char -> Int -> Maybe (Smix, (Char, Int))
+dealWith :: Bool -> Char -> Int -> (Smix, (Char, Int))
 dealWith invert k x
-    | x == 1 = Nothing
-    | invert = Just (RightSmix, (k,x))
-    | otherwise = Just (LeftSmix, (k,x))
+    | invert = (RightSmix, (k,x))
+    | otherwise = (LeftSmix, (k,x))
 
-dealWithBoth :: Char -> Int -> Int -> Maybe (Smix, (Char, Int))
-dealWithBoth k 1 1 = Nothing
-dealWithBoth k x 1 = Just (LeftSmix, (k, x))
-dealWithBoth k 1 y = Just (RightSmix, (k, y))
+dealWithBoth :: Char -> Int -> Int -> (Smix, (Char, Int))
+dealWithBoth k x 1 = (LeftSmix, (k, x))
+dealWithBoth k 1 y = (RightSmix, (k, y))
 dealWithBoth k x y
-    | x == y = Just (EqSmix, (k, x))
-    | x > y = Just (LeftSmix, (k, x))
-    | x < y = Just (RightSmix, (k, y))
+    | x == y = (EqSmix, (k, x))
+    | x > y = (LeftSmix, (k, x))
+    | x < y = (RightSmix, (k, y))
 
 generalOrdering :: (Smix, (Char, Int)) -> (Smix, (Char, Int)) -> Ordering
 generalOrdering l@(ls, (k1, v1)) r@(rs, (k2,v2))
@@ -46,9 +44,9 @@ mix :: String -> String -> String
 mix s1 s2 = intercalate "/" (showSmix <$> sortedBySmix)
     where countMapLeft = charCountMap $ filter isLower s1
           countMapRight = charCountMap $ filter isLower s2
-          mergeMap = Merge.merge (Merge.mapMaybeMissing (dealWith False))
-                                 (Merge.mapMaybeMissing (dealWith True))
-                                 (Merge.zipWithMaybeMatched dealWithBoth)
+          mergeMap = Merge.merge (Merge.mapMissing (dealWith False))
+                                 (Merge.mapMissing (dealWith True))
+                                 (Merge.zipWithMatched dealWithBoth)
                                  countMapLeft countMapRight
           result = snd <$> Map.toList mergeMap
-          sortedBySmix = sortBy generalOrdering result
+          sortedBySmix = sortBy generalOrdering $ filter ((> 1) . snd . snd) result
