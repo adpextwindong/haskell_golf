b94a5ea655d71ed3f3242add053495861d2ff304
Takumi Crary <tcrary@gmail.com>
2021-03-02 08:02:45 +0000

    Add WIP subHeap processing to BestTravel kata

--- CodeWars/WIP_BestTravel.hs
+++ CodeWars/WIP_BestTravel.hs
@@ -24,15 +24,43 @@ costMap cost 1 xs = IMap.fromAscList baseSums
     where filteredByCost = filter (<= cost) xs
           baseSums = zip filteredByCost $ fmap Set.singleton [1..]
 
-costHeap :: Int -> Int -> [Int] -> Maybe Int
-costHeap cost 1 xs = fmap fst resulthead
+type SumMaxHeapWInd = Heap.HeapT (Heap.Prio Heap.FstMaxPolicy (Int, Set.Set Int)) (Set.Set Int)
+type SumWIndItem = (Int, Set.Set Int)
+
+costHeap :: Int -> Int -> [Int] -> SumMaxHeapWInd
+costHeap _ _ [] = Heap.empty
+costHeap cost 1 xs = tail_candidates 
     where ts = zip xs (Set.singleton <$> [1..])
           hs = Heap.fromList ts :: Heap.MaxPrioHeap Int (Set.Set Int)
-          candidates = Heap.break (\(p,s) -> p <= cost) hhs
-          resulthead = Heap.viewHead . snd $ candidates
+          (prefix, tail_candidates) = Heap.break (\(p,s) -> p <= cost) hhs
 
-costHeap cost n (x:xs) = undefined
+costHeap cost n xs = undefined
+    where ts = zip xs [1..]
+          subCostHeap = costHeap cost (n-1) xs
+          candidate_heaps = fmap (proccessSubHeap subCostHeap cost) ts
+          result = Heap.unions candidate_heaps
+          
+-- Heap.break (\(p,s) p + x <= cost && x notMember s) (costHeap cost (n-1) xs)
+-- union (fmap (\(x,ind) ))
 
-tts = zip [50..55] (Set.singleton <$> [1..])
-hhs = Heap.fromList tts :: Heap.MaxPrioHeap Int (Set.Set Int)
-ccs = 53
+-- Heap.break for viable sums, then fmap the new sum operand and index into the set
+proccessSubHeap :: SumMaxHeapWInd -> Int -> (Int, Int) -> SumMaxHeapWInd
+proccessSubHeap = undefined
+-- TODO fix this
+-- proccessSubHeap subCostHeap cost choice@(x, index) = result
+--     where (_, candidate_heap) = Heap.break (pViableSum cost choice) subCostHeap :: ([SumWIndItem], SumMaxHeapWInd)
+--           result = fmap (newSum choice) candidate_heap
+
+pViableSum :: Int -> (Int, Int) -> (Int, Set.Set Int) -> Bool
+pViableSum cost (x, index) (p,s) = p + x <= cost && (Set.notMember x s)
+
+newSum :: (Int, Int) -> (Int, Set.Set Int) -> (Int, Set.Set Int)
+newSum (x, index) (p,s) = (x+p, Set.insert index s)
+
+pluckSum :: SumMaxHeapWInd -> Maybe Int 
+pluckSum = fmap fst . Heap.viewHead
+
+bts = zip [50..55] (Set.singleton <$> [1..])
+ts = zip [70..75] [1..]
+hhs = Heap.fromList bts :: Heap.MaxPrioHeap Int (Set.Set Int)
+ccs = 127
