88290e437056a57dddee9c2e5c592d62b60e96ac
Takumi Crary <tcrary@gmail.com>
2021-03-16 06:26:25 +0100

    Golf one line out of bob

--- exercism/haskell/bob/src/Bob.hs
+++ exercism/haskell/bob/src/Bob.hs
@@ -3,12 +3,11 @@ module Bob (responseFor) where
 import Data.Char
 
 responseFor :: String -> String
-responseFor [] = "Fine. Be that way!"
 responseFor s
+    | pSilence = "Fine. Be that way!"
     | pYell alphaOnly && not (null alphaOnly) = if pQuestion s
                      then "Calm down, I know what I'm doing!"
                      else "Whoa, chill out!"
-    | pSilence = "Fine. Be that way!"
     | pQuestion s = "Sure."
     | otherwise = "Whatever."
   where
