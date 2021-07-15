5fa98284a2d76fae2d133d0ec7df419569de2abe
Takumi Crary <tcrary@gmail.com>
2021-07-15 18:52:10 +0900

    Add final working version of tick esolang

--- CodeWars/esolang_tick.hs
+++ CodeWars/esolang_tick.hs
@@ -5,7 +5,8 @@ import Data.Char
 import Control.Monad.Writer
 
 interpreter :: String -> String
-interpreter tape = undefined -- fmap command tape TODO
+interpreter tape = execWriter $ exec initialState command_stream
+    where command_stream = fmap command tape
 
 data Command = MoveL | MoveR | Incr | Put
 
@@ -17,18 +18,22 @@ data MachineState = MachineState {
 initialState = MachineState 0 (IntMap.fromList [])
 
 exec :: MachineState -> [Command] -> Writer String ()
+exec _ [] = return ()
 exec (MachineState ptr mem) (MoveL:ts) = exec (MachineState (ptr - 1) mem) ts
 exec (MachineState ptr mem) (MoveR:ts) = exec (MachineState (ptr + 1) mem) ts
+
 exec (MachineState ptr mem) (Incr:ts) = exec (MachineState ptr newMem) ts
     where newMem = IntMap.insertWith wrap ptr 1 mem
           wrap :: Int -> Int -> Int
           wrap x y = if x + y > 255
                      then x + y - 255
                      else x + y
-exec (MachineState ptr mem) (Put:ts) = do
+
+exec m@(MachineState ptr mem) (Put:ts) = do
     case IntMap.lookup ptr mem of
-        Just val -> tell $ [chr val]
-        Nothing -> return ()
+        Just val -> tell [chr val]
+        Nothing -> tell [chr 0]
+    exec m ts
 
 command :: Char -> Command
 command '<' = MoveL
